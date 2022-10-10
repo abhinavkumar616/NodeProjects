1092ce286e0f621c0783aed07d653003eda65bfd
Abhinav <abhinavkumar616@gmail.com>
2022-10-11 01:10:22 +0530

    nodeproject

---
+++ CHALK/index.js
@@ -0,0 +1,7 @@
+// chalk Module
+
+import chalk from "chalk";
+console.log(chalk.bgBlue("Hello World"));
+console.log(chalk.bgCyan("Hello World1"));
+console.log(chalk.bgGreenBright("Hello World2"));
+console.log(chalk.bgCyanBright("Hello World3"));
